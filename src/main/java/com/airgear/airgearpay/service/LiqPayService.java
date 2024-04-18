1f006b79e368dffec811c4c80a4cf74e8ac4cc9e
Ilya Deus <deus.ilya@gmail.com>
2024-04-19 02:45:53 +0300

    start

---
+++ src/main/java/com/airgear/airgearpay/service/LiqPayService.java
@@ -0,0 +1,13 @@
+package com.airgear.airgearpay.service;
+
+import com.airgear.dto.CheckoutDto;
+import com.airgear.dto.GoodsDto;
+import org.springframework.security.core.Authentication;
+
+public interface LiqPayService {
+
+    CheckoutDto createCheckoutDtoPay(GoodsDto goods, Authentication auth);
+
+    String generatePaymentLink(CheckoutDto checkoutDTO) throws IllegalAccessException;
+
+}
